1035e6b41eaa0e664755302b711ed62074ee797b
unknown <shivamnagpal432@gmail.com>
2019-04-06 15:43:00 +0530

    ScreenWrap generalized

--- Assets/_Scripts/ScreenWrap.cs
+++ Assets/_Scripts/ScreenWrap.cs
@@ -4,28 +4,39 @@ using UnityEngine;
 
 public class ScreenWrap : MonoBehaviour
 {
-    private readonly int error = 10;
+    private readonly float error = 0.01f;
+    private Collider collider;
+
+    private void Start()
+    {
+        collider = this.GetComponent<Collider>();
+    }
 
     private void OnTriggerExit(Collider other)
     {
         if (other.gameObject.CompareTag("ScreenBounds"))
         {
-            Vector3 position = other.transform.InverseTransformVector(this.transform.position);
-            
-            if (Mathf.Abs(position.x) > 0.5)
+            Vector3 position = this.transform.position;
+            Vector3 inverseTransformPosition = other.transform.InverseTransformVector(position);
+
+            if (Mathf.Abs(inverseTransformPosition.x) > 0.5)
             {
-                position.x *= -1;
-                position.x += -Mathf.Sign(position.x) * error / Screen.width;
+                Vector3 point = other.transform.InverseTransformVector(collider.ClosestPoint(new Vector3(0, position.y, 0)));
+                float offset = Mathf.Abs(point.x) - 0.5f;
+                inverseTransformPosition.x *= -1;
+                inverseTransformPosition.x += -Mathf.Sign(inverseTransformPosition.x) * (offset + error);
             }
 
-            if (Mathf.Abs(position.y) > 0.5)
+            if (Mathf.Abs(inverseTransformPosition.y) > 0.5)
             {
-                position.y *= -1;
-                position.y += -Mathf.Sign(position.y) * error / Screen.height;
+                Vector3 point = other.transform.InverseTransformVector(collider.ClosestPoint(new Vector3(position.x, 0, 0)));
+                float offset = Mathf.Abs(point.y) - 0.5f;
+                inverseTransformPosition.y *= -1;
+                inverseTransformPosition.y += -Mathf.Sign(inverseTransformPosition.y) * (offset + error);
             }
 
-            position = other.transform.TransformVector(position);
-            this.transform.position = position;
+            inverseTransformPosition = other.transform.TransformVector(inverseTransformPosition);
+            this.transform.position = inverseTransformPosition;
         }
     }
 }
